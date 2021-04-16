0d004fc735730aeced976cb1cdd386673fd4eb6b
sdsd <sdsd>
2021-04-16 09:50:17 +0700

    ver 5

--- main.js
+++ main.js
@@ -1,182 +1,34 @@
-// // function button() {
+$('.object_logo .col-flex').slick({
 
-
-// //     // var x, i;
-// //     // x = document.querySelectorAll(".btn");
-// //     // for (i = 0; i < x.length; i++) {
-// //     //     x[i].style.backgroundColor = "red";
-// //     //     x[i].st
-// //     // }
-// //     document.querySelector("#bt1").style.backgroundColor = "gray";
-// //     document.querySelector("#bt1").style.color = "white";
-// //     document.querySelector("#oj1").style.display = "none";
-// //     document.querySelector("#more").innerHTML = "-"
-// //     document.querySelector("#bt1").id = "bt11";
-
-
-
-// //     document.querySelector("#bt11").onclick = function() { myFunction1() };
-
-// // }
-
-
-
-
-// // function myFunction1() {
-
-// //     document.querySelector("#bt11").style.backgroundColor = "gray";
-// //     document.querySelector("#bt11").style.color = "white";
-// //     document.querySelector("#oj1").style.display = "block";
-// //     document.querySelector("#more").innerHTML = "+"
-
-// //     // if (document.querySelector("#oj1").style.display = "block") {
-// //     //     document.querySelector("#bt1").style.backgroundColor = "white";
-// //     //     document.querySelector("#bt1").style.color = "black";
-// //     //     document.querySelector("#oj1").style.display = "none";
-// //     //     document.querySelector("#more").innerHTML = "+";
-
-// //     // }
-// // }
-
-// // bt1
-// $(document).ready(function() {
-//     $("#bt1").click(function() {
-//         $("#oj1").hide();
-//         $("#oj2").hide();
-//         $("#oj3").hide();
-//         $("#oj4").hide();
-//         $("#oj5").hide();
-//         $("#oj6").hide();
-//         document.querySelector("#bt1").style.display = "none";
-//         document.querySelector("#bt11").style.display = "block";
-//     });
-// });
-// $(document).ready(function() {
-//     $("#bt11").click(function() {
-//         document.querySelector("#oj1").style.display = "block";
-//         document.querySelector("#bt1").style.display = "block";
-//         document.querySelector("#bt11").style.display = "none";
-//     });
-// });
-// // bt2
-// $(document).ready(function() {
-//     $("#bt2").click(function() {
-//         $("#oj1").hide();
-
-//         $("#oj3").hide();
-//         $("#oj4").hide();
-//         $("#oj5").hide();
-//         $("#oj6").hide();
-//         document.querySelector("#oj2").style.display = "block";
-//         document.querySelector("#bt2").style.display = "none";
-//         document.querySelector("#bt22").style.display = "block";
-//     });
-// });
-// $(document).ready(function() {
-//     $("#bt22").click(function() {
-//         $("#oj2").hide();
-
-//         document.querySelector("#bt2").style.display = "block";
-//         document.querySelector("#bt22").style.display = "none";
-//     });
-// });
-// // bt2
-// $(document).ready(function() {
-//     $("#bt3").click(function() {
-//         $("#oj1").hide();
-//         $("#oj2").hide();
-//         $("#oj4").hide();
-//         $("#oj5").hide();
-//         $("#oj6").hide();
-//         document.querySelector("#oj3").style.display = "block";
-//         document.querySelector("#bt3").style.display = "none";
-//         document.querySelector("#bt33").style.display = "block";
-//     });
-// });
-// $(document).ready(function() {
-//     $("#bt33").click(function() {
-//         $("#oj3").hide();
-//         document.querySelector("#bt3").style.display = "block";
-//         document.querySelector("#bt33").style.display = "none";
-//     });
-// });
-// //bt3
-// $(document).ready(function() {
-//     $("#bt3").click(function() {
-//         $("#oj1").hide();
-//         $("#oj2").hide();
-//         $("#oj4").hide();
-//         $("#oj5").hide();
-//         $("#oj6").hide();
-//         document.querySelector("#oj3").style.display = "block";
-//         document.querySelector("#bt3").style.display = "none";
-//         document.querySelector("#bt33").style.display = "block";
-//     });
-// });
-// $(document).ready(function() {
-//     $("#bt33").click(function() {
-//         $("#oj3").hide();
-//         document.querySelector("#bt3").style.display = "block";
-//         document.querySelector("#bt33").style.display = "none";
-//     });
-// });
-// //bt4
-// $(document).ready(function() {
-//     $("#bt4").click(function() {
-//         $("#oj1").hide();
-//         $("#oj2").hide();
-//         $("#oj3").hide();
-//         $("#oj5").hide();
-//         $("#oj6").hide();
-//         document.querySelector("#oj4").style.display = "block";
-//         document.querySelector("#bt4").style.display = "none";
-//         document.querySelector("#bt44").style.display = "block";
-//     });
-// });
-// $(document).ready(function() {
-//     $("#bt44").click(function() {
-//         $("#oj4").hide();
-//         document.querySelector("#bt4").style.display = "block";
-//         document.querySelector("#bt44").style.display = "none";
-//     });
-// });
-// //bt5
-// $(document).ready(function() {
-//     $("#bt4").click(function() {
-//         $("#oj1").hide();
-//         $("#oj2").hide();
-//         $("#oj3").hide();
-//         $("#oj5").hide();
-//         $("#oj6").hide();
-//         document.querySelector("#oj4").style.display = "block";
-//         document.querySelector("#bt4").style.display = "none";
-//         document.querySelector("#bt44").style.display = "block";
-//     });
-// });
-// $(document).ready(function() {
-//     $("#bt44").click(function() {
-//         $("#oj4").hide();
-//         document.querySelector("#bt4").style.display = "block";
-//         document.querySelector("#bt44").style.display = "none";
-//     });
-// });
-// //bt6
-// $(document).ready(function() {
-//     $("#bt4").click(function() {
-//         $("#oj1").hide();
-//         $("#oj2").hide();
-//         $("#oj3").hide();
-//         $("#oj5").hide();
-//         $("#oj6").hide();
-//         document.querySelector("#oj4").style.display = "block";
-//         document.querySelector("#bt4").style.display = "none";
-//         document.querySelector("#bt44").style.display = "block";
-//     });
-// });
-// $(document).ready(function() {
-//     $("#bt44").click(function() {
-//         $("#oj4").hide();
-//         document.querySelector("#bt4").style.display = "block";
-//         document.querySelector("#bt44").style.display = "none";
-//     });
-// });
+    speed: 300,
+    lazyLoad: 'ondemand',
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
