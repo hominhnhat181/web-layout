6ff732e55c822686c1d95c76008ed42c2344f6b9
sdsd <sdsd>
2021-04-19 08:09:49 +0700

    ver 9

--- main.js
+++ main.js
@@ -32,3 +32,34 @@ $('.object_logo .col-flex').slick({
         // instead of a settings object
     ]
 });
+
+
+
+// ssssssssssssss
+const accordionHeadingEls = document.querySelectorAll(".accordion__heading");
+
+function accordion() {
+    for (let item of accordionHeadingEls) {
+        let accordionIcon = item.lastElementChild;
+
+        item.addEventListener("click", function() {
+            let accordionDetails = this.nextElementSibling;
+            item.classList.toggle("accordion__heading--active");
+            for (let item2 of accordionHeadingEls) {
+                if (item2 != item) {
+                    item2.nextElementSibling.style.maxHeight = null;
+                    item2.lastElementChild.classList.remove("accordion__icon--turn");
+                    item2.classList.remove('accordion__heading--active');
+                }
+            }
+
+            accordionIcon.classList.toggle("accordion__icon--turn");
+            if (accordionDetails.style.maxHeight != 0) {
+                accordionDetails.style.maxHeight = null;
+            } else {
+                accordionDetails.style.maxHeight = accordionDetails.scrollHeight + "px";
+            }
+        });
+    }
+}
+accordion();
